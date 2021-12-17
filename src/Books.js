28f3ce046d05f249724bf7c74709d5edc7adbb80
Chaz Carothers <chaz.carothers@gmail.com>
2021-12-16 22:07:01 -0600

    initial commit

---
+++ src/Books.js
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const res = await axios.get(
+        `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_NYT_API_KEY}`
+      );
+      console.log(res.data);
+    };
+    fetchBooks();
+  }, []);
+  return (
+    <div>
+      <h1>Books</h1>
+    </div>
+  );
+};
+
+export default Books;
