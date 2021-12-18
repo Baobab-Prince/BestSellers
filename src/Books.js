62f5d4e733f598c86bb1a7a562320a7faecad2ce
Chaz Carothers <chaz.carothers@gmail.com>
2021-12-18 17:29:22 -0600

    updated results

--- src/Books.js
+++ src/Books.js
@@ -7,9 +7,9 @@ const Books = () => {
   useEffect(() => {
     const fetchBooks = async () => {
       const res = await axios.get(
-        `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_NYT_API_KEY}`
+        `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_BOOKS_API_KEY}`
       );
-      console.log(res.data);
+      console.log(res.data.results.books);
     };
     fetchBooks();
   }, []);
