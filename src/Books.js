182775454fa883db51a84914b74af09d9a6dc30f
Chaz Carothers <chaz.carothers@gmail.com>
2021-12-19 19:55:57 -0600

    styled headings/lists

--- src/Books.js
+++ src/Books.js
@@ -16,8 +16,8 @@ const Books = () => {
   }, []);
   return (
     <>
-      <h1>Books</h1>
-      <section>
+      <h1 className="font-bold text-center text-4xl py-5">Books</h1>
+      <section className="grid grid-cols-1 gap-10 px-5">
         {books.map((book) => {
           const {
             author,
@@ -29,24 +29,36 @@ const Books = () => {
             rank,
             title,
           } = book;
+
           return (
             <article key={rank}>
               <div>
-                <img src={book_image} alt={title} />
+                <img
+                  src={book_image}
+                  alt={title}
+                  className="block mx-auto w-1/2"
+                />
               </div>
 
               <div>
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <p>{author}</p>
+                <h3 className="font-bold my-2 text-2xl">{title}</h3>
+                <p className="mb-4">{description}</p>
+                <p>
+                  <span className="font-bold">Author:</span> {author}
+                </p>
               </div>
-              <ul>
-                <li>Publisher: {publisher}</li>
-                <li>ISBN: {primary_isbn10}</li>
+
+              <ul className="mb-4">
+                <li>
+                  <span className="font-bold">Publisher:</span> {publisher}
+                </li>
+                <li>
+                  <span className="font-bold">ISBN:</span> {primary_isbn10}
+                </li>
               </ul>
 
               <ul>
-                <p>Buy Now:</p>
+                <h3 className="font-bold text-xl">Buy Now:</h3>
                 {buy_links.map((link) => {
                   const { name, url } = link;
                   return (
